5dc20ad282a103cb01296a21ec72049a510930ad
Natalia Polishchuk <Natalia@Natalias-MacBook-Pro.local>
2016-07-15 14:11:05 -0700

    ready for heroku deploy attempt #1

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
+mongoose.connect( process.env.MONGODB_URI || "https://warm-bastion-71807.herokuapp.com/");
 
 // module.exports.Campsite = require("./campsite.js.example");
